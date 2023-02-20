d9eadeeb090a922b0115c1812bb8cf1ae44c9e0d
michmattera <michellemattera97@gmail.com>
2023-02-20 16:31:04 +0000

    Add function to get weight from user and give errors if invalid data

--- run.py
+++ run.py
@@ -11,13 +11,9 @@ SCOPE = [
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('VetSeed')
+SHEET = GSPREAD_CLIENT.open('VetSeed').sheet1
 
-profile = SHEET.worksheet('profile')
-
-data = profile.get_all_values()
-
-print(data)
+print(SHEET.cell(1,1).value)
 
 
 def introduction():
@@ -25,24 +21,69 @@ def introduction():
     define goal of program
     Create function for introduction of topic
     """
-    
+    #profile = SHEET.worksheet('profile')
+    #data = profile.get_all_values()
+    # print(data)
     print("Welcome to VetSeed, program to authomate data for all vets.")
-    print("Answer the next questions to calcolate dog's per day calories")
+    print("Answer the next questions to calcolate dog's per day calories\n")
     while True:
         print("Please insert first name of dog")
         print("Example: Bob\n")
 
         data_name = input("Name of dog:\n")
-
         saved_name = data_name
 
         if validate_info(saved_name):
             print(f"Thank you")
-            print(f"Name of dog provided is {saved_name}")
-            print(f"Saving name ...")
+            print(f"Name of dog provided is {saved_name}\n")
             break
 
     return saved_name
+
+
+def dogs_weight():
+    """
+    Ask user to insert weight of dog
+    Check that right input was provided
+    """
+    print("Please provide weight of dog")
+    print("Please provide exact weight from 0 to 100 kg")
+    while True:
+        data_weight = input("Weight of dog:")
+        saved_weight = data_weight
+
+        if validate_weight(saved_weight):
+            print(f"Weight of dog provided is {saved_weight}")
+            break
+    return saved_weight
+
+
+def validate_weight(saved_weight):
+    """
+    Validate weight
+    check if value was between 0 and 100
+    check if values was not a letter
+    give error if invalid error
+    """
+    try:
+        [int(value) for value in saved_weight]
+        if int(saved_weight) < 0 or int(saved_weight) > 100:
+            raise ValueError(
+                f"Please insert value between 0 and 100"
+            )
+            return False
+
+        if not saved_weight:
+            raise ValueError(
+                f"Field cannot be left blank"
+            )
+            return False
+
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
+        return False
+
+    return True
 
 
 def validate_info(values):
@@ -58,24 +99,24 @@ def validate_info(values):
             )
             return False
 
-        elif not values:
+        if not values:
             raise ValueError(
                 f"Field cannot be left blank"
             )
             return False
+            
+            
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
         return False
 
-    except error as e :
-        print(f"Field cannot be left blank")
-        return False
     return True
 
 
 def main():
     introduction()
+    dogs_weight()
     
 
-
 main()
+
